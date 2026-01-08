e67e4d88eca5d1cfab9a67563b4380e03d538bf8
cropsbreaker <valefalco11@gmail.com>
2026-01-08 18:45:15 +0100

    Added a control to unclock the coclea

--- stove_logic.h
+++ stove_logic.h
@@ -3,157 +3,168 @@
 #include <cmath>
 
 inline void run_stove_control_loop() {
-  
-  if (!id(stove_pwr_status).state) return;
-
-
-  uint32_t now = millis();
-  float T_smoke = id(temp_smoke).state;
-  float T_water = id(temp_water).state;
-
-  // 1. SANITY CHECK (Sensor Health) ---
-  if (std::isnan(T_smoke) || T_smoke < 0) {
-    id(status_message) = "ERROR_SMOKE_SENSOR_FAIL";
-    ESP_LOGE("SAFETY", "CRITICAL: Sonda Fumi Fail!");
-    id(critical_exit).execute();
-    return;
-  }
-
-  // 2. STARTUP LOGIC
-  if (!id(launch_status)) {
-    // Case A. Fire detected
-    if (T_smoke >= id(smoke_start_threshold)) {
-      id(launch_status) = true;
-      id(led_ok).turn_on();
-      
-      // Anti-Windup at startup
-      id(lastTime) = now;
-      id(prev_error) = id(T_target) - T_water; 
-      id(integral) = 0.0; 
-      
-      id(status_message) = "RUN_OK";
-      ESP_LOGW("STATE_MGR", ">>> FIRE DETECTED. Entering RUN Mode. <<<");
-      return;
+  if(id(anticlogging_mode).state) {
+    static uint32_t last_pulse_start = 0;
+    uint32_t now = millis();
+    
+    if (now - last_pulse_start >= 12000) {
+      id(coclea_pulse).execute(6000);
+      last_pulse_start = now;
     }
+        return;
+  } else {
+    // In Anti-Clogging mode, disable stove control
+    if (!id(stove_pwr_status).state) return;
 
-    // Case B. Timeout
-    if ((now - id(starting_time)) >= id(launch_time_cycle)) {
-      id(status_message) = "ERROR_STARTUP_TIMEOUT";
-      ESP_LOGE("STATE_MGR", "BOOT FAILED: Timeout.");
+
+    uint32_t now = millis();
+    float T_smoke = id(temp_smoke).state;
+    float T_water = id(temp_water).state;
+
+    // 1. SANITY CHECK (Sensor Health) ---
+    if (std::isnan(T_smoke) || T_smoke < 0) {
+      id(status_message) = "ERROR_SMOKE_SENSOR_FAIL";
+      ESP_LOGE("SAFETY", "CRITICAL: Sonda Fumi Fail!");
       id(critical_exit).execute();
       return;
     }
 
-    // Case C. Waiting (UI Update)
-    if (!id(boot_sequence).is_running()) {
-      id(status_message) = "START_WATING_FIRE";
+    // 2. STARTUP LOGIC
+    if (!id(launch_status)) {
+      // Case A. Fire detected
+      if (T_smoke >= id(smoke_start_threshold)) {
+        id(launch_status) = true;
+        id(led_ok).turn_on();
+        
+        // Anti-Windup at startup
+        id(lastTime) = now;
+        id(prev_error) = id(T_target) - T_water; 
+        id(integral) = 0.0; 
+        
+        id(status_message) = "RUN_OK";
+        ESP_LOGW("STATE_MGR", ">>> FIRE DETECTED. Entering RUN Mode. <<<");
+        return;
+      }
+
+      // Case B. Timeout
+      if ((now - id(starting_time)) >= id(launch_time_cycle)) {
+        id(status_message) = "ERROR_STARTUP_TIMEOUT";
+        ESP_LOGE("STATE_MGR", "BOOT FAILED: Timeout.");
+        id(critical_exit).execute();
+        return;
+      }
+
+      // Case C. Waiting (UI Update)
+      if (!id(boot_sequence).is_running()) {
+        id(status_message) = "START_WATING_FIRE";
+      }
+
+
+      // Case D. Fire detected but is small
+      if(!id(boot_sequence).is_running() && T_smoke >= 45.0f) {
+        ESP_LOGW("STATE_MGR", ">>> SMALL FIRE DETECTED. Continuing BOOT SEQUENCE. <<<");
+        id(coclea_pulse).execute((int)(8 * 1000));
+      }
+      return;
     }
 
+    // --- 3. RUN LOGIC & SAFETY ---
 
-    // Case D. Fire detected but is small
-    if(!id(boot_sequence).is_running() && T_smoke >= 45.0f) {
-      ESP_LOGW("STATE_MGR", ">>> SMALL FIRE DETECTED. Continuing BOOT SEQUENCE. <<<");
-      id(coclea_pulse).execute((int)(8 * 1000));
+    // Safety: Loss of Flame
+    if (T_smoke < id(smoke_min_run)) {
+      id(status_message) = "ERROR_FLAME_LOSS";
+      ESP_LOGE("SAFETY", "FLAME LOSS detected.");
+      id(critical_exit).execute();
+      return;
     }
-    return;
-  }
 
-  // --- 3. RUN LOGIC & SAFETY ---
+    // Safety: Smoke Overheat
+    if (T_smoke > id(smoke_max_safety)) {
+      id(status_message) = "ERROR_SMOKE_OVERHEAT";
+      ESP_LOGE("SAFETY", "OVERHEAT SMOKE.");
+      id(critical_exit).execute();
+      return;
+    }
 
-  // Safety: Loss of Flame
-  if (T_smoke < id(smoke_min_run)) {
-    id(status_message) = "ERROR_FLAME_LOSS";
-    ESP_LOGE("SAFETY", "FLAME LOSS detected.");
-    id(critical_exit).execute();
-    return;
-  }
+    // Safety: Water Overheat
+    if (!std::isnan(T_water) && T_water > 85.0) {
+      id(status_message) = "ERROR_WATER_OVERHEAT";
+      ESP_LOGE("SAFETY", "OVERHEAT WATER.");
+      id(critical_exit).execute();
+      return;
+    }
 
-  // Safety: Smoke Overheat
-  if (T_smoke > id(smoke_max_safety)) {
-    id(status_message) = "ERROR_SMOKE_OVERHEAT";
-    ESP_LOGE("SAFETY", "OVERHEAT SMOKE.");
-    id(critical_exit).execute();
-    return;
-  }
+    id(status_message) = "RUN_OK";
 
-  // Safety: Water Overheat
-  if (!std::isnan(T_water) && T_water > 85.0) {
-    id(status_message) = "ERROR_WATER_OVERHEAT";
-    ESP_LOGE("SAFETY", "OVERHEAT WATER.");
-    id(critical_exit).execute();
-    return;
-  }
+    // --- 4. PID LOOP & ACTUATOR CONTROL ---
+    
+    uint32_t dt_ms = now - id(lastTime);
+    
+    // Sampling Enforcement: Execute PID only every cycleTime
+    if (dt_ms < id(cycleTime)) return;
 
-  id(status_message) = "RUN_OK";
+    float dt = dt_ms / 1000.0f; // Seconds
+    id(lastTime) = now;
 
-  // --- 4. PID LOOP & ACTUATOR CONTROL ---
-  
-  uint32_t dt_ms = now - id(lastTime);
-  
-  // Sampling Enforcement: Execute PID only every cycleTime
-  if (dt_ms < id(cycleTime)) return;
+    float error = id(T_target) - T_water;
+    if (std::isnan(error)) return;
 
-  float dt = dt_ms / 1000.0f; // Seconds
-  id(lastTime) = now;
+    // A. PID
 
-  float error = id(T_target) - T_water;
-  if (std::isnan(error)) return;
+    // Integral
+    id(integral) += error * dt;
+    id(integral) = fmaxf(fminf(id(integral), 50.0f), -50.0f);
 
-  // A. PID
+    // Derivative
+    float derivative = (error - id(prev_error)) / dt;
+    id(prev_error) = error;
 
-  // Integral
-  id(integral) += error * dt;
-  id(integral) = fmaxf(fminf(id(integral), 50.0f), -50.0f);
+    // PID output(normalized 0.0 - 1.0+)
+    float pid_power = (id(K_p) * error) + (id(K_i) * id(integral)) + (id(K_d) * derivative);
 
-  // Derivative
-  float derivative = (error - id(prev_error)) / dt;
-  id(prev_error) = error;
+    // Output Saturation (0.0 - 1.2 for aggressive boost, but min 0)
+    pid_power = fmaxf(fminf(pid_power, 1.2f), 0.0f);
+    
+    
+    // B. "Smart Bucket" Logic
+    static float pellet_bucket = 0.0f;
 
-  // PID output(normalized 0.0 - 1.0+)
-  float pid_power = (id(K_p) * error) + (id(K_i) * id(integral)) + (id(K_d) * derivative);
+    // Anti-starvation system
+    const float MIN_KEEP_ALIVE = 0.125f; // 2/16s minimum to keep fire alive
+    
+    pid_power = fmaxf(fminf(pid_power, 1.2f), MIN_KEEP_ALIVE);
 
-  // Output Saturation (0.0 - 1.2 for aggressive boost, but min 0)
-  pid_power = fmaxf(fminf(pid_power, 1.2f), 0.0f);
-  
-  
-  // B. "Smart Bucket" Logic
-  static float pellet_bucket = 0.0f;
+    if (pid_power > 0) {
+        pellet_bucket += pid_power * dt; 
+    }
 
-  // Anti-starvation system
-  const float MIN_KEEP_ALIVE = 0.125f; // 2/16s minimum to keep fire alive
-  
-  pid_power = fmaxf(fminf(pid_power, 1.2f), MIN_KEEP_ALIVE);
+    // Clamp Bucket (Max 10s reserve)
+    pellet_bucket = fminf(pellet_bucket, 10.0f);
+    
+    // C. Actuation
+    const float MIN_MOTOR_ON = 2.0f; // Minimum relay activation time
+    const float MIN_REST_TIME = 1.5f; // Minimum time to let the fire burn before re-adding pellets(avoid pellet hills)
 
-  if (pid_power > 0) {
-      pellet_bucket += pid_power * dt; 
-  }
+    float t_on = 0.0f;
 
-  // Clamp Bucket (Max 10s reserve)
-  pellet_bucket = fminf(pellet_bucket, 10.0f);
-  
-  // C. Actuation
-  const float MIN_MOTOR_ON = 2.0f; // Minimum relay activation time
-  const float MIN_REST_TIME = 1.5f; // Minimum time to let the fire burn before re-adding pellets(avoid pellet hills)
+    if (pellet_bucket >= MIN_MOTOR_ON) {
+        float max_run_this_cycle = dt - MIN_REST_TIME;
+        t_on = fminf(pellet_bucket, max_run_this_cycle);
+        pellet_bucket -= t_on;
+    } else if(T_smoke < 130.0f){
+      t_on = fminf(t_on, dt * 0.5f); // Avoid a complete shutdown. 100 is too low
+    }
 
-  float t_on = 0.0f;
+    
 
-  if (pellet_bucket >= MIN_MOTOR_ON) {
-      float max_run_this_cycle = dt - MIN_REST_TIME;
-      t_on = fminf(pellet_bucket, max_run_this_cycle);
-      pellet_bucket -= t_on;
-  } else if(T_smoke < 130.0f){
-    t_on = fminf(t_on, dt * 0.5f); // Avoid a complete shutdown. 100 is too low
-  }
+    // Debug Telemetry
+    ESP_LOGD("PID_CTRL", "Err:%.1f | P:%.2f I:%.2f D:%.2f | Out:%.2f | Buck:%.2fs | ACT:%.2fs", 
+            error, (id(K_p)*error), (id(K_i)*id(integral)), (id(K_d)*derivative), 
+            pid_power, (pellet_bucket + t_on), t_on);
 
-  
-
-  // Debug Telemetry
-  ESP_LOGD("PID_CTRL", "Err:%.1f | P:%.2f I:%.2f D:%.2f | Out:%.2f | Buck:%.2fs | ACT:%.2fs", 
-           error, (id(K_p)*error), (id(K_i)*id(integral)), (id(K_d)*derivative), 
-           pid_power, (pellet_bucket + t_on), t_on);
-
-  if (t_on > 0.0f) {
-    // Call the script defined in YAML passing the milliseconds
-    id(coclea_pulse).execute((int)(t_on * 1000));
+    if (t_on > 0.0f) {
+      // Call the script defined in YAML passing the milliseconds
+      id(coclea_pulse).execute((int)(t_on * 1000));
+    }
   }
 }
